0e42d56978447328f6252db24147734cc7432207
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2023-02-13 14:20:06 +0100

    :sparkles: lang

---
+++ components/sprache.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Sprache = () => {
+  return (
+    <section className="wrapper" id="sprache" aria-label="Sprache abschnitt">
+        <h2>Sprachkenntnisse</h2>
+        <div className='lang'>
+            <h3>Deutsch</h3>
+            <p>Muttersprache</p>
+        </div>
+        <div className='lang'>
+            <h3>Englisch</h3>
+            <p>Verhandlungssicher (mündliche und schriftlich)</p>
+        </div>
+        <div className='lang'>
+            <h3>Koreanisch</h3>
+            <p>Gute Kenntnisse (mündlich und schriftlich)</p>
+        </div>  
+    </section>
+  )
+}
+
+export default Sprache
